openfde/lineageos_android_system_core

(waydroid) init: modify mount_all to skip mounts and trigger nonencrypted state
Change-Id: Iae8cffa6aeb2d56f35a51f0e9f52fd92402c43e9

init/builtins.cpp
@@ -655,7 +655,8 @@ static Result<void> do_mount_all(const BuiltinArguments& args) {
     std::string prop_name = "ro.boottime.init.mount_all."s + prop_post_fix;
     android::base::Timer t;
 
-    Fstab fstab;
+    // Waydroid: mounts are handled by LXC container
+    /*Fstab fstab;
     if (mount_all->fstab_path.empty()) {
         if (!ReadDefaultFstab(&fstab)) {
             return Error() << "Could not read default fstab";
@@ -664,14 +665,17 @@ static Result<void> do_mount_all(const BuiltinArguments& args) {
         if (!ReadFstabFromFile(mount_all->fstab_path, &fstab)) {
             return Error() << "Could not read fstab";
         }
-    }
+    }*/
 
-    auto mount_fstab_result = fs_mgr_mount_all(&fstab, mount_all->mode);
+    /*auto mount_fstab_result = fs_mgr_mount_all(&fstab, mount_all->mode);*/
     SetProperty(prop_name, std::to_string(t.duration().count()));
 
     if (mount_all->import_rc) {
         import_late(mount_all->rc_paths);
     }
+
+    // Waydroid: filesystem mount is handled outside container, always non-encrypted
+    MountAllResult mount_fstab_result = {FS_MGR_MNTALL_DEV_NOT_ENCRYPTED, true};
 
     if (mount_fstab_result.userdata_mounted) {
         // This call to fs_mgr_mount_all mounted userdata. Keep the result in

WayDroid-ATV/android_system_core

init/builtins.cpp
@@ -650,7 +650,8 @@ static Result<void> do_mount_all(const BuiltinArguments& args) {
     std::string prop_name = "ro.boottime.init.mount_all."s + prop_post_fix;
     android::base::Timer t;
 
-    Fstab fstab;
+    // Waydroid: mounts are handled by LXC container
+    /*Fstab fstab;
     if (mount_all->fstab_path.empty()) {
         if (!ReadDefaultFstab(&fstab)) {
             return Error() << "Could not read default fstab";
@@ -659,14 +660,17 @@ static Result<void> do_mount_all(const BuiltinArguments& args) {
         if (!ReadFstabFromFile(mount_all->fstab_path, &fstab)) {
             return Error() << "Could not read fstab";
         }
-    }
+    }*/
 
-    auto mount_fstab_result = fs_mgr_mount_all(&fstab, mount_all->mode);
+    /*auto mount_fstab_result = fs_mgr_mount_all(&fstab, mount_all->mode);*/
     SetProperty(prop_name, std::to_string(t.duration().count()));
 
     if (mount_all->import_rc) {
         import_late(mount_all->rc_paths);
     }
+
+    // Waydroid: filesystem mount is handled outside container, always non-encrypted
+    MountAllResult mount_fstab_result = {FS_MGR_MNTALL_DEV_NOT_ENCRYPTABLE, true};
 
     if (mount_fstab_result.userdata_mounted) {
         // This call to fs_mgr_mount_all mounted userdata. Keep the result in